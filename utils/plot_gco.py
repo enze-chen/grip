87877ef25f6c4518719dfde0718a4bb0a5bfc65c
Enze Chen <enze.chen1@gmail.com>
2025-05-03 12:03:48 -0700

    updated example params.yaml files

--- utils/plot_gco.py
+++ utils/plot_gco.py
@@ -8,13 +8,15 @@ from unique import clear_best, process_output
 
 #############################################################################
 
-def make_plot(dir_best: str = "best", hide: bool = False) -> None:
+def make_plot(dir_best: str = "best", Emax: float = 0.0, hide: bool = False) -> None:
     """
     Make a GCO plot (Egb vs. n) for the best structures.
 
     Args:
         dir_best (str, optional): Folder to save structures in.
         Defaults to "best".
+
+        Emax (float, optiona): Maximum value on y-axis.
 
         hide (bool, optional): Whether to hide the rendered Pyplot.
         Defaults to False.
@@ -29,8 +31,9 @@ def make_plot(dir_best: str = "best", hide: bool = False) -> None:
     fig, ax = plt.subplots(layout="constrained")
     ax.plot(ngb_list, Egb_list, 'o', c='k', ms=3, clip_on=True)
     ymin = ax.get_ylim()[0]
+    ymax = Emax if Emax else Egbmaxmin + 0.2 * (Egbmaxmin - ymin)
     ax.set(xlim=(0, 1),
-           ylim=(ymin, Egbmaxmin + 0.2 * (Egbmaxmin - ymin)),
+           ylim=(ymin, ymax),
            xlabel=r"$[n]$", ylabel=r"$E_{\mathrm{gb}}$ (J m$^{-2}$)")
 
     fig.savefig(f"{dir_best}/best_grip_plot.png", dpi=300, bbox_inches="tight")
@@ -61,6 +64,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plot GCO plot (Egb vs. n) for GRIP results.")
     parser.add_argument("-f", "--folder", type=str, default="best",
                         help="Folder where GB structs are stored.")
+    parser.add_argument("-E", "--Emax", type=float, 
+                        help="Max Egb for y axis of plot.")
     parser.add_argument("-s", "--silent", action="store_true",
                         help="Do not show the plot.")
     parser.add_argument("-e", "--extra", action="store_true",
@@ -69,6 +74,7 @@ if __name__ == "__main__":
                         help="View a structure instead of the GCO plot.")
     args = parser.parse_args()
     dir_best = args.folder
+    Emax = args.Emax
     hide = args.silent
     extra = args.extra
     filename = args.file
@@ -78,4 +84,4 @@ if __name__ == "__main__":
     if filename:
         view_struct(filename)
     else:
-        make_plot(dir_best, hide)
+        make_plot(dir_best, Emax, hide)
